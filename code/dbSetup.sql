02b408da0a32b14417c228cee4b68f0eea8c5ec0
Ethan Aymeric Schafstall <po01imj@eduvaud.ch>
2024-05-29 17:19:03 +0200

    Changed dbSetup data

--- code/dbSetup.sql
+++ code/dbSetup.sql
@@ -11,35 +11,6 @@ CREATE TABLE t_users (
    PRIMARY KEY (userId)
 );
 
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('eli123', 'g4oC7GKB0HA2u0E', 'admin');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('appleeater', 'qeyYlesGjCDeTvw', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('etest9', 'LY7nzY90cccTpLc', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('darknight', 'QP6CZ_EQ04u8uZG', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('spiderman123', 'D50HsYCOCfODbtN', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('csgoofer', 'XM7WHKAtptFlLgW', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('username123', 'Lno0MJ9ZtIXPrSu', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('betamale2', 'n1GtHexXi7HkGuM', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('evanyeagar', 'ZPKFoJnpX_6FBlD', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('ethan', 'password', 'user');
-CREATE DATABASE IF NOT EXISTS db_test;
+INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('etml', 'etml', 'user', '258912b7f291d7b5bc32c21618d658473aec00ce85670bf217cf125b002ff377', 'PSaHAjZx9bffU5');
 
-USE db_test;
 
-CREATE TABLE t_users (
-   userId INT AUTO_INCREMENT,
-   useUsername VARCHAR(50) NOT NULL,
-   usePassword VARCHAR(50) NOT NULL,
-   useRole VARCHAR(50) NOT NULL,
-   PRIMARY KEY (userId)
-);
-
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('eli123', 'g4oC7GKB0HA2u0E', 'admin');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('appleeater', 'qeyYlesGjCDeTvw', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('etest9', 'LY7nzY90cccTpLc', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('darknight', 'QP6CZ_EQ04u8uZG', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('spiderman123', 'D50HsYCOCfODbtN', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('csgoofer', 'XM7WHKAtptFlLgW', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('username123', 'Lno0MJ9ZtIXPrSu', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('betamale2', 'n1GtHexXi7HkGuM', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('evanyeagar', 'ZPKFoJnpX_6FBlD', 'user');
-INSERT INTO t_users (useUsername, usePassword, useRole) VALUES ('ethan', 'password', 'user');
