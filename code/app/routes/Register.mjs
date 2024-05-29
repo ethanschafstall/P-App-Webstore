122d76521bb5bfa4c34353555927c3c547c23054
Ethan Aymeric Schafstall <po01imj@eduvaud.ch>
2024-05-29 17:21:23 +0200

    updated register route

--- code/app/routes/Register.mjs
+++ code/app/routes/Register.mjs
@@ -2,7 +2,9 @@
 import express from "express";
 import jwt from 'jsonwebtoken';
 import { connectToDatabase } from "../tools/sqlConnection.mjs";
-import { hash } from "../tools/hash.mjs";
+import { generateHash } from "../tools/hash.mjs";
+import { CheckDataType } from "../tools/TypeChecker.mjs";
+
 const registerRoute = express(); // Creating a new instance of express router
 
 const connectToDatabaseMiddleware = async (req, res, next) => {
@@ -13,39 +15,32 @@ const connectToDatabaseMiddleware = async (req, res, next) => {
       console.error("Error connecting to the database:", error);
       res.status(500).json({ error: "Internal Server Error" });
     }
-  };
+};
 
 // Endpoint for handling user login
 registerRoute.post('/', connectToDatabaseMiddleware, async (req, res) => {
-    const { username, password, role} = req.body;
-    const [hashedPassword, salt] = hash(password);
+  const { username, password, role } = req.body;
+  const [hashedPassword, salt] = generateHash(password);
 
-    if (!CheckDataType(username, "string") || !CheckDataType(role, "string")) {
-        return res.status(401).json({ error: "Invalid value types" });
+  if (!CheckDataType(username, "string") || !CheckDataType(role, "string")) {
+    return res.status(401).json({ error: "Invalid value types" });
+  }
+
+  const queryString = `INSERT INTO t_users (useUsername, usePassword, useRole, useSalt) VALUES (?, ?, ?, ?)`;
+
+  try {
+    const [result] = await req.dbConnection.execute(queryString, [username, hashedPassword, role, salt]);
+
+    if (result.affectedRows === 1) {
+      const message = `User has successfully registered.`;
+      return res.status(200).json({ message });
+    } else {
+      res.status(401).json({ error: "Failed to register user." });
     }
-    const queryString2 = `Insert Into t_users (useUsername, usePassword, useRole, useSalt) Values (?,?,?,?)`;
-    try {
-      const [rows] = await req.dbConnection.execute(queryString2, [username, hashedPassword, role, salt]);
-      if (rows.length < 0) {
-        
-  
-        const message = `user has successfully connected`;
-      return res.status(200).json({message});
-  
-        
-  
-      } else {
-        console.log(rows)
-        res.status(401).json({ error: "Invalid username or password" });
-      }
-    } catch (error) {
-      console.error("Error authenticating user:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-const CheckDataType = (value, type) => {
-
-    return typeof value === type
-}
 export { registerRoute }; // Exporting the router for use in other files
