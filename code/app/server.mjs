4f8865f050937af6e4b0308f880db5dd187ed7c3
ethanschafstall <ethans9999@gmail.com>
2024-05-31 16:33:20 +0200

    updated server mjs with https

--- code/app/server.mjs
+++ code/app/server.mjs
@@ -1,12 +1,23 @@
 
 
+
 // Librairies et ressources
+
+import https from 'node:https';
+import http from 'node:http';
+import fs from 'node:fs';
 
 import express from "express";
 import userRoute from "./routes/User.mjs";
-import { loginRoute } from "./routes/Login.mjs";
+import { loginRoute } from "./routes/login.mjs";
 import { registerRoute } from "./routes/Register.mjs";
 import adminRoute from "./routes/Admin.mjs";
+
+
+const options = {
+  key: fs.readFileSync('openssl/privateKey.key'),
+  cert: fs.readFileSync('openssl/certificate.crt'),
+};
 
 
 const app = express();
@@ -21,8 +32,18 @@ app.use('/user', userRoute);
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
 
-let port = 443;
+let portHttp = 443;
+let portHttps = 4433;
 // Démarrage du serveur
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+
+ https.createServer(options, app).listen(1111, () => {
+    console.log(`Server HTTPS running on https://localhost:${portHttps}`)
+});
+ 
+http.createServer(app).listen(portHttp, () => {
+    console.log(`Server HTTP running on http://localhost:${portHttp}`)
 });
